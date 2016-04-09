d6ba028d971990602082ee27ec450369b3085ab4
LuigiVampa <maxschip2306@mail.ru>
2016-04-09 16:12:06 +0300

    Created structure of the project. Started creating UI. User created.

---
+++ Source/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace Model
+{
+    public class User
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Soname { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Initials
+        {
+            get
+            {
+                var firstNameLetter = Name.First();
+                var firstLastNameLetter = LastName.First();
+                return firstNameLetter + "." + firstLastNameLetter;
+            }
+        }
+
+        public string Position { get; set; }
+    }
+}
